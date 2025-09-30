47d97deaad3b94d4dc58ed48f1d8aa26ec2067eb
victorisimo <vhernandez@nabenik.dev>
2025-09-29 18:37:23 -0600

    first commit

---
+++ src/test/java/com/victorisimo/platzi_play/PlatziPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorisimo.platzi_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
