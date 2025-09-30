5f7dbcaa446eecd822353f93d4a0b9d356515fc3
victorisimo <vhernandez@nabenik.dev>
2025-09-29 19:31:36 -0600

    add application-dev properties, application-prod properties and files configurations

--- src/main/java/com/victorisimo/platzi_play/HelloController.java
+++ src/main/java/com/victorisimo/platzi_play/HelloController.java
@@ -1,18 +1,23 @@
 package com.victorisimo.platzi_play;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private final String platform;
     private final PlatziPlayAiService aiService;
 
-    public HelloController(PlatziPlayAiService aiService) {
+
+    public HelloController(@Value("${spring.application.name}") String platform, PlatziPlayAiService aiService) {
+        this.platform = platform;
         this.aiService = aiService;
     }
 
     @GetMapping("/")
     public String hello() {
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(this.platform);
     }
 }
--- src/main/java/com/victorisimo/platzi_play/PlatziPlayAiService.java
+++ src/main/java/com/victorisimo/platzi_play/PlatziPlayAiService.java
@@ -1,15 +1,16 @@
 package com.victorisimo.platzi_play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlatziPlayAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de Gestión de Películas PlatziPlay.
+            Genera un saludo de bienvenida a la plataforma de Gestión de Películas {{platform}}.
             Usa menos de 120 caracteres y hazlo con un tono amigable y entusiasta.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform);
 
 }
