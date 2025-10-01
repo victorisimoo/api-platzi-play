07c59bd2e8fabe74811fbc3b2cfe1fcad6ee91c0
victorisimo <vhernandez@nabenik.dev>
2025-09-30 23:47:59 -0600

    Add movie entity

---
+++ src/main/java/com/victorisimo/platzi_play/persistence/entity/MovieEntity.java
@@ -0,0 +1,90 @@
+package com.victorisimo.platzi_play.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 150, unique = true)
+    private String title;
+
+    @Column(name = "timeMinutes", nullable = false, precision = 3)
+    private Integer timeMinutes;
+
+    @Column(name = "gender", nullable = false, length = 40)
+    private String gender;
+
+    @Column(name = "releaseDate")
+    private LocalDate releaseDate;
+
+    @Column(name = "rating", precision = 2, scale = 1)
+    private BigDecimal rating;
+
+    @Column(name = "status", nullable = false, length = 1)
+    private String status;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getTimeMinutes() {
+        return timeMinutes;
+    }
+
+    public void setTimeMinutes(Integer timeMinutes) {
+        this.timeMinutes = timeMinutes;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public BigDecimal getRating() {
+        return rating;
+    }
+
+    public void setRating(BigDecimal rating) {
+        this.rating = rating;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
